d5b866c1d1cbeaee71aee6852e766ac57d6703f4
zomerk <zomerfeldkacper@gmail.com>
2024-04-16 21:33:02 +0200

    comments

--- CaloriesCounterAPI/DTO/EditMealDTO.cs
+++ CaloriesCounterAPI/DTO/EditMealDTO.cs
@@ -1,18 +1,37 @@
 ﻿using CaloriesCounterAPI.Models;
+using System;
+using System.Collections.Generic;
 
 namespace CaloriesCounterAPI.DTO
 {
+    /// <summary>
+    /// Data transfer object for editing a meal.
+    /// </summary>
     public class EditMealDTO
     {
+        /// <summary>
+        /// Gets or sets the unique identifier of the meal.
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the type of the meal.
+        /// </summary>
         public MealType Type { get; set; }
 
+        /// <summary>
+        /// Gets or sets the date of the meal.
+        /// </summary>
         public DateOnly Date { get; set; }
+
+        /// <summary>
+        /// Gets or sets the IDs of the products to be included in the meal.
+        /// </summary>
         public List<int>? ProductIds { get; set; }
 
-        public List<int>? AmmoutOfProduct { get; set; }
-
-
+        /// <summary>
+        /// Gets or sets the amounts of each product to be included in the meal.
+        /// </summary>
+        public List<int>? AmountOfProduct { get; set; }
     }
 }
--- CaloriesCounterAPI/Models/Product.cs
+++ CaloriesCounterAPI/Models/Product.cs
@@ -2,19 +2,47 @@
 using Microsoft.AspNetCore.OpenApi;
 using Microsoft.AspNetCore.Http.HttpResults;
 using System.Text.Json.Serialization;
+
 namespace CaloriesCounterAPI.Models
 {
+    /// <summary>
+    /// Represents a product in the calorie counter system.
+    /// </summary>
     public class Product
     {
+        /// <summary>
+        /// Gets or sets the unique identifier of the product.
+        /// </summary>
         public int ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the product.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the calorie content of the product.
+        /// </summary>
         public int Kcal {  get; set; }
 
+        /// <summary>
+        /// Gets or sets the fat content of the product.
+        /// </summary>
         public int Fat { get; set; }
 
+        /// <summary>
+        /// Gets or sets the carbohydrates content of the product.
+        /// </summary>
         public int Carbs { get; set; }
 
+        /// <summary>
+        /// Gets or sets the protein content of the product.
+        /// </summary>
         public int Protein { get; set; }
+
+        /// <summary>
+        /// Gets or sets the meals associated with the product.
+        /// </summary>
         [JsonIgnore]
         public ICollection<Meal> Meals { get; set; }
     }
